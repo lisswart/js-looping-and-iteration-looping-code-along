0c4cd5c3abb6d7f6f7a0824a52803487ade80ce4
lisswart <arnoudlisa@gmail.com>
2021-03-31 02:46:01 -0400

    commit tree 8f6f9173564f720ca539d61286ece134cbc603c8

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(nameArr, event) {
+    let thankYouCards = [];
+    for(let i=0; i < nameArr.length; i++) {
+        thankYouCards = thankYouCards.concat(`Thank you, ${nameArr[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCards;
+}
+ console.log(writeCards(['Ada', 'Lina', 'Greg'], "surprise"));
+
+ function countDown(num) {
+     while(num >= 0) {
+         console.log(num);
+         num--;
+     }
+ }
